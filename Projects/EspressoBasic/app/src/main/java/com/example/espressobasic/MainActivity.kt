8dc8e970d4e58e79097f268771ba7e8a82bc9d5d
Devrath <devrath.dev595@gmail.com>
2021-05-17 17:35:18 +0530

    c

--- Projects/EspressoBasic/app/src/main/java/com/example/espressobasic/MainActivity.kt
+++ Projects/EspressoBasic/app/src/main/java/com/example/espressobasic/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.espressobasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var greetingView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        greetingView = findViewById(R.id.greeting)
+    }
+
+    fun greet(v: View) {
+        greetingView.setText(R.string.hello)
     }
 }
