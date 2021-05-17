f2429a4c81d5c5e6975232979460db605a950d81
Devrath <devrath.dev595@gmail.com>
2021-05-17 11:19:45 +0530

    m

--- Projects/RecepieApp/app/src/main/java/com/example/effectivetesting/ui/MainActivity.kt
+++ Projects/RecepieApp/app/src/main/java/com/example/effectivetesting/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.effectivetesting
+package com.example.effectivetesting.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.effectivetesting.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
