a11dc10c810d7e172c5304b64f432d136d0f19ea
Devrath <devrath.dev595@gmail.com>
2021-06-25 10:09:58 +0530

    v

--- Projects/MockKApp/app/src/main/java/com/example/code/StartActivity.kt
+++ Projects/MockKApp/app/src/main/java/com/example/code/StartActivity.kt
@@ -12,7 +12,7 @@ class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
