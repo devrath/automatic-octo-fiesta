005f0515c827b60fe24c460345bc6dbf8e10c50e
Devrath <devrath.dev595@gmail.com>
2021-06-27 20:35:11 +0530

    v

--- Projects/EspressoDemos/EspressoEffectiveTesting/app/src/test/java/com/example/code/data/RecipeTest.java
+++ Projects/EspressoDemos/EspressoEffectiveTesting/app/src/test/java/com/example/code/data/RecipeTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.InputStream;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 public class RecipeTest {
 
@@ -13,6 +14,9 @@ public class RecipeTest {
         InputStream stream = RecipeTest.class.getResourceAsStream("/recipes/water.txt");
         Recipe recipe = Recipe.readFromStream(stream);
         assertNotNull(recipe);
+        assertEquals("water",recipe.id);
+        assertEquals("Water",recipe.title);
+        assertEquals("Put glass under tap. Open tap. Close tap. Drink.",recipe.description);
     }
 
 }
