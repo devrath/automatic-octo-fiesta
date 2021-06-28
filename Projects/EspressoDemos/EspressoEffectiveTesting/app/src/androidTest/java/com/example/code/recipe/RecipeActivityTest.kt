4a3b9a5922f2335ba733f4b5a33c8c7f7d84e826
Devrath <devrath.dev595@gmail.com>
2021-06-28 17:48:46 +0530

    v

--- Projects/EspressoDemos/EspressoEffectiveTesting/app/src/androidTest/java/com/example/code/recipe/RecipeActivityTest.kt
+++ Projects/EspressoDemos/EspressoEffectiveTesting/app/src/androidTest/java/com/example/code/recipe/RecipeActivityTest.kt
@@ -5,11 +5,17 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isSelected
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.code.R
+import com.example.code.recipe.RecipeActivity.KEY_ID
 import org.hamcrest.CoreMatchers
 import org.junit.After
 import org.junit.Before
@@ -21,11 +27,11 @@ import org.junit.runner.RunWith
 class RecipeActivityTest {
 
     private lateinit var scenario: ActivityScenario<RecipeActivity>
+    private lateinit var intent : Intent
 
     @Before
     fun setup() {
-        val intent = Intent(ApplicationProvider.getApplicationContext(), RecipeActivity::class.java)
-        scenario = launchActivity(intent)
+        intent = Intent(ApplicationProvider.getApplicationContext(), RecipeActivity::class.java)
     }
 
     @After
@@ -34,12 +40,39 @@ class RecipeActivityTest {
     }
 
     @Test
-    fun recipeNotFound() {
+    fun checkRecipeIsNotFound() {
+        // Launch the activity
+        scenario = launchActivity(intent)
         // Check if the default text view notification is displayed
         Espresso.onView(ViewMatchers.withId(R.id.description))
             .check(ViewAssertions.matches(ViewMatchers.withText(R.string.recipe_not_found)))
-        // Check if the the title text view is not displayed - this id is from recycler view row
+        // Check if the the title text view is not displayed
         Espresso.onView(ViewMatchers.withId(R.id.title))
             .check(ViewAssertions.matches(CoreMatchers.not(ViewMatchers.isDisplayed())))
     }
+
+    @Test
+    fun checkRecipeIsFound() {
+        // Add appropriate value to intent
+        intent.putExtra(KEY_ID,"chocolate_pudding")
+        // Launch the activity
+        scenario = launchActivity(intent)
+        // Check if the recipe title and id is displayed
+        Espresso.onView(ViewMatchers.withId(R.id.title))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Chocolate Pudding")))
+    }
+
+    @Test
+    fun checkOnClickFunctionalityOfFavourite() {
+        // Add appropriate value to intent
+        intent.putExtra(KEY_ID,"chocolate_pudding")
+        // Launch the activity
+        scenario = launchActivity(intent)
+        // Check if the recipe title and id is displayed
+        Espresso.onView(ViewMatchers.withId(R.id.title))
+            .check(matches(withText("Chocolate Pudding")))
+            .perform(click())
+            .check(matches(isSelected()))
+    }
+
 }
