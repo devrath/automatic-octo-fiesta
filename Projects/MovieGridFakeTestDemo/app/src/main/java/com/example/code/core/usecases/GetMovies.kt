7f49f3d555eceee2ad9e663044fa2a914e49e94a
Devrath <devrath.dev595@gmail.com>
2021-07-02 10:36:44 +0530

    m

--- Projects/MovieGridFakeTestDemo/app/src/main/java/com/example/code/core/usecases/GetMovies.kt
+++ Projects/MovieGridFakeTestDemo/app/src/main/java/com/example/code/core/usecases/GetMovies.kt
@@ -5,6 +5,10 @@ import com.example.code.core.repositories.MoviesRepository
 import com.example.code.models.Movie
 import javax.inject.Inject
 
+/**
+ * According to our convention use case must be run in background
+ * This use case is run in the background and returns the result
+ * **/
 class GetMovies
 @Inject constructor(private val moviesRepository: MoviesRepository) : UseCase<List<Movie>, UseCase.None>() {
 
