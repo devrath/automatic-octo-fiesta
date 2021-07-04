ab72a2e0237540d5d08c28c846c226348cdb09b9
Devrath <devrath.dev595@gmail.com>
2021-07-04 09:44:54 +0530

    d

--- Projects/MovieGridFakeTestDemo/app/src/androidTest/java/com/example/code/base/BaseTest.kt
+++ Projects/MovieGridFakeTestDemo/app/src/androidTest/java/com/example/code/base/BaseTest.kt
@@ -1,9 +1,13 @@
 package com.example.code.base
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.code.utils.FileReader
 import com.example.code.utils.OkHttpIdlingResourceRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -19,5 +23,28 @@ abstract class BaseTest {
     val portNumber = 8080
 
     val mockWebServer = MockWebServer()
+
+    @Before
+    @Throws
+    fun setup() {
+        mockWebServer.start(portNumber)
+    }
+
+    @After
+    @Throws
+    fun teardown() {
+        mockWebServer.shutdown()
+    }
+
+
+    fun successfulServerResponse(inputJsonPath : String) {
+        mockWebServer.dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return MockResponse()
+                    .setResponseCode(200)
+                    .setBody(FileReader.readStringFromFile("success_response.json"))
+            }
+        }
+    }
 
 }
--- Projects/MovieGridFakeTestDemo/app/src/androidTest/java/com/example/code/feature/MainActivityTest.kt
+++ Projects/MovieGridFakeTestDemo/app/src/androidTest/java/com/example/code/feature/MainActivityTest.kt
@@ -1,60 +1,24 @@
 package com.example.code.feature
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import com.example.code.utils.FileReader
-import com.example.code.utils.OkHttpIdlingResourceRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import okhttp3.mockwebserver.Dispatcher
-import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.MockWebServer
-import okhttp3.mockwebserver.RecordedRequest
-import org.junit.After
-import org.junit.Before
+import com.example.code.base.BaseTest
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@HiltAndroidTest
-@RunWith(AndroidJUnit4::class)
-class MainActivityTest {
+class MainActivityTest : BaseTest(){
 
-    @get:Rule
-    var rule = OkHttpIdlingResourceRule()
-
-    val portNumber = 8080
-
-    val mockWebServer = MockWebServer()
 
     @get:Rule
     val activityRule = ActivityTestRule(MainActivity::class.java, true, false)
 
-    @Before
-    @Throws
-    fun setup() {
-        mockWebServer.start(portNumber)
-    }
 
     @Test
     fun testForSuccessfulResponse() {
-        successfulServerResponse()
+        successfulServerResponse("success_response.json")
         activityRule.launchActivity(null)
     }
 
-    private fun successfulServerResponse() {
-        mockWebServer.dispatcher = object : Dispatcher() {
-            override fun dispatch(request: RecordedRequest): MockResponse {
-                return MockResponse()
-                    .setResponseCode(200)
-                    .setBody(FileReader.readStringFromFile("success_response.json"))
-            }
-        }
-    }
 
-    @After
-    @Throws
-    fun teardown() {
-        mockWebServer.shutdown()
-    }
+
 }
 
