fc227d115dd98b658b902b6be3c9010e86b2a989
Devrath <devrath.dev595@gmail.com>
2021-07-08 18:09:30 +0530

    v

--- Projects/MovieGridFakeTestDemo/app/src/test/java/com/example/code/core/repositories/FakeMoviesRepository.kt
+++ Projects/MovieGridFakeTestDemo/app/src/test/java/com/example/code/core/repositories/FakeMoviesRepository.kt
@@ -17,11 +17,16 @@ class FakeMoviesRepository constructor(
     private var movieList = listOf(Movie(1,"SomeUrl"))
 
     override fun movies(): Either<Failure, List<Movie>> {
-        return when (cases){
-            FakeMoviesRepositoryCases.EMPTY_MOVIE_LIST -> Either.Right(emptyList())
-            FakeMoviesRepositoryCases.MOVIE_LIST -> Either.Right(movieList)
-            FakeMoviesRepositoryCases.FAILURE_CONNECTIVITY -> Either.Left(Failure.NetworkConnection)
-            FakeMoviesRepositoryCases.FAILURE_SERVER_ERROR -> Either.Left(Failure.ServerError)
+
+        return if(!networkHandler.isNetworkAvailable()){
+            Either.Left(Failure.NetworkConnection)
+        }else{
+            when (cases){
+                FakeMoviesRepositoryCases.EMPTY_MOVIE_LIST -> Either.Right(emptyList())
+                FakeMoviesRepositoryCases.MOVIE_LIST -> Either.Right(movieList)
+                FakeMoviesRepositoryCases.FAILURE_CONNECTIVITY -> Either.Left(Failure.NetworkConnection)
+                FakeMoviesRepositoryCases.FAILURE_SERVER_ERROR -> Either.Left(Failure.ServerError)
+            }
         }
     }
 }
--- Projects/MovieGridFakeTestDemo/app/src/test/java/com/example/code/core/usecases/GetMoviesUseCaseTest.kt
+++ Projects/MovieGridFakeTestDemo/app/src/test/java/com/example/code/core/usecases/GetMoviesUseCaseTest.kt
@@ -6,16 +6,20 @@ import com.example.code.core.exception.Failure
 import com.example.code.core.functional.Either
 import com.example.code.core.interactor.UseCase
 import com.example.code.core.network.MoviesApi
+import com.example.code.core.platform.FakeNetworkHandler
 import com.example.code.core.platform.NetworkHandler
 import com.example.code.core.repositories.FakeMoviesRepository
 import com.example.code.core.repositories.FakeMoviesRepositoryCases
 import com.example.code.core.repositories.MoviesRepository
+import com.example.code.core.services.FakeMoviesService
 import com.example.code.core.services.MoviesService
 import com.example.code.models.Movie
 import com.nhaarman.mockitokotlin2.mock
+import junit.framework.Assert.assertTrue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockWebServer
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,12 +35,15 @@ class GetMoviesUseCaseTest : BaseUnitTest() {
     @Test
     fun `test for successful movies list retrieval from web service`() = runBlocking{
 
-        val fakeRepo = FakeMoviesRepository(mock{},mock{}, FakeMoviesRepositoryCases.MOVIE_LIST)
+        val fakeRepo = FakeMoviesRepository(
+            FakeNetworkHandler(mock{},isNetAvailable = true),
+            FakeMoviesService(),
+            FakeMoviesRepositoryCases.MOVIE_LIST)
+
         val useCase = GetMoviesUseCase(fakeRepo)
 
         useCase(UseCase.None(), this) {
-            println(""+it)
-            assert(true) { it.isRight }
+            assertTrue(it.isRight)
         }
     }
 
@@ -44,12 +51,15 @@ class GetMoviesUseCaseTest : BaseUnitTest() {
     @Test
     fun `test for empty movies list retrieval from web service`() = runBlocking{
 
-        val fakeRepo = FakeMoviesRepository(mock{},mock{}, FakeMoviesRepositoryCases.EMPTY_MOVIE_LIST)
+        val fakeRepo = FakeMoviesRepository(
+            FakeNetworkHandler(mock{},isNetAvailable = true),
+            FakeMoviesService(),
+            FakeMoviesRepositoryCases.EMPTY_MOVIE_LIST)
+
         val useCase = GetMoviesUseCase(fakeRepo)
 
         useCase(UseCase.None(), this) {
-            println(""+it)
-            assert(true) { it.isLeft }
+            assertTrue(it.isRight)
         }
     }
 
@@ -57,12 +67,15 @@ class GetMoviesUseCaseTest : BaseUnitTest() {
     @Test
     fun `test for error in connectivity`() = runBlocking{
 
-        val fakeRepo = FakeMoviesRepository(mock{},mock{}, FakeMoviesRepositoryCases.FAILURE_CONNECTIVITY)
+        val fakeRepo = FakeMoviesRepository(
+            FakeNetworkHandler(mock{},isNetAvailable = false),
+            FakeMoviesService(),
+            FakeMoviesRepositoryCases.FAILURE_CONNECTIVITY)
+
         val useCase = GetMoviesUseCase(fakeRepo)
 
         useCase(UseCase.None(), this) {
-            println(""+it)
-            assert(true) { it.isLeft }
+            assertTrue(it.isLeft)
         }
     }
 
@@ -70,12 +83,15 @@ class GetMoviesUseCaseTest : BaseUnitTest() {
     @Test
     fun `test for error in server`() = runBlocking{
 
-        val fakeRepo = FakeMoviesRepository(mock{},mock{}, FakeMoviesRepositoryCases.FAILURE_SERVER_ERROR)
+        val fakeRepo = FakeMoviesRepository(
+            FakeNetworkHandler(mock{},isNetAvailable = true),
+            FakeMoviesService(),
+            FakeMoviesRepositoryCases.FAILURE_SERVER_ERROR)
+
         val useCase = GetMoviesUseCase(fakeRepo)
 
         useCase(UseCase.None(), this) {
-            println(""+it)
-            assert(true) { it.isLeft }
+            assertTrue(it.isLeft)
         }
     }
 
