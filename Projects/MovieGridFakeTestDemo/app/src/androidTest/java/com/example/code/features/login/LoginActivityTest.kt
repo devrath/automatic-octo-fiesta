11f5843816aff474a9aa2d73d02f68fb6723595a
Devrath <devrath.dev595@gmail.com>
2021-07-10 17:06:29 +0530

    v

--- Projects/MovieGridFakeTestDemo/app/src/androidTest/java/com/example/code/features/login/LoginActivityTest.kt
+++ Projects/MovieGridFakeTestDemo/app/src/androidTest/java/com/example/code/features/login/LoginActivityTest.kt
@@ -1,5 +1,122 @@
 package com.example.code.features.login
 
+import android.content.Context
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.core.app.launchActivity
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.code.R
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class LoginActivityTest
+@RunWith(AndroidJUnit4::class)
+class LoginActivityTest{
+    private lateinit var activityScenario: ActivityScenario<LoginActivity>
+
+    @Before
+    fun setUp() {
+        launchActivityUnderTest()
+    }
+
+    @After
+    fun tearDown() {
+        activityScenario.close()
+    }
+
+    @Test
+    fun checkForEmptyUserName() {
+
+        launchActivityUnderTest()
+
+        // ARRANGE
+        val userName = ""
+        val password = "pass"
+
+        // ACT
+        actOnLogin(userName, password)
+
+        // ASSERT
+        assertOnErrorText(getString(R.string.msg_username_empty))
+    }
+
+    @Test
+    fun checkForEmptyPassword() {
+        // ARRANGE
+        val userName = "mail@example.com"
+        val password = ""
+
+        // ACT
+        actOnLogin(userName, password)
+
+        // ASSERT
+        assertOnErrorText(getString(R.string.msg_password_empty))
+    }
+
+    @Test
+    fun check_scenario_when_user_name_is_invalid() {
+        // ARRANGE
+        val userName = "wrong@example.com"
+        val password = "pass"
+
+        // ACT
+        actOnLogin(userName, password)
+
+        // ASSERT
+        assertOnErrorText(getString(R.string.msg_username_incorrect))
+    }
+
+    @Test
+    fun check_scenario_when_password_is_invalid() {
+        // ARRANGE
+        val userName = "mail@example.com"
+        val password = "wrong password"
+
+        // ACT
+        actOnLogin(userName, password)
+
+        // ASSERT
+        assertOnErrorText(getString(R.string.msg_password_incorrect))
+    }
+
+    @Test
+    fun check_scenario_for_successful_login() {
+        // ARRANGE
+        val userName = "mail@example.com"
+        val password = "pass"
+
+        // ACT
+        actOnLogin(userName, password)
+
+        // ASSERT
+        login {
+            matchText(R.id.tvName, getString(R.string.name_surname))
+        }
+    }
+
+    private fun actOnLogin(userName: String, password: String) {
+        login {
+            setEmail(userName)
+            setPassword(password)
+            clickLogin()
+        }
+    }
+
+    private fun assertOnErrorText(message: String) {
+        login { matchErrorText(message) }
+    }
+
+    /**
+     * Launch the screen
+     */
+    private fun launchActivityUnderTest() {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), LoginActivity::class.java)
+        activityScenario = launchActivity(intent)
+    }
+
+    private fun getString(res: Int): String = ApplicationProvider.getApplicationContext<Context>().getString(res)
+
+}
