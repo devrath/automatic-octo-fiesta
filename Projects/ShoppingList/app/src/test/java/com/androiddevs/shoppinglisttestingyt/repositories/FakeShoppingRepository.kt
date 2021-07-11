749149892da9ce4b21d232688578efced14106fd
Devrath <devrath.dev595@gmail.com>
2021-07-11 09:49:52 +0530

    v

--- Projects/ShoppingList/app/src/test/java/com/androiddevs/shoppinglisttestingyt/repositories/FakeShoppingRepository.kt
+++ Projects/ShoppingList/app/src/test/java/com/androiddevs/shoppinglisttestingyt/repositories/FakeShoppingRepository.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
 import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
-import com.androiddevs.shoppinglisttestingyt.di.Resource
+import com.androiddevs.shoppinglisttestingyt.other.Resource
 
 class FakeShoppingRepository : ShoppingRepository {
 
