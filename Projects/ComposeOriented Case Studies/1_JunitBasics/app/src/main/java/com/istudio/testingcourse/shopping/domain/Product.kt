1968dfdcd75173fcf696bff731c1225794684668
DevIndiGG <devrath.ad@indi.gg>
2023-10-23 17:32:42 +0530

    removed unused project

--- Projects/ComposeOriented Case Studies/1_JunitBasics/app/src/main/java/com/istudio/testingcourse/shopping/domain/Product.kt
+++
@@ -1,7 +0,0 @@
-package com.istudio.testingcourse.shopping.domain
-
-data class Product(
-    val id: Int,
-    val name: String,
-    val price: Double,
-)
--- Projects/ComposeOriented Case Studies/1_JunitBasics/app/src/main/java/com/istudio/testingcourse/shopping/domain/ShoppingCartCache.kt
+++
@@ -1,9 +0,0 @@
-package com.istudio.testingcourseexamples.part1.domain
-
-import com.istudio.testingcourse.shopping.domain.Product
-
-interface ShoppingCartCache {
-    fun saveCart(items: List<Product>)
-    fun loadCart(): List<Product>
-    fun clearCart()
-}
